d5d828e08854e23de9be2e63118fb37f19c85b61
Roel van Dijk <roel@rustradio.org>
2012-01-17 18:39:37 +0100

    Add specs for uploading.

--- spec/flickr_uploader_spec.rb
+++ spec/flickr_uploader_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 describe FlickrUploader::Uploader do
 
   before do
-    @photo_double1 = double
-    @photo_double1.stub(:photoid).and_return(42)
+    @uploader = double
+    Flickr::Uploader.stub(:new).and_return(@uploader)
 
-    @uploader_double = double
-    Flickr::Uploader.stub(:new).and_return(@uploader_double)
+    @photo1 = create_photo(42, "photo001")
+    @photo2 = create_photo(43, "photo002")
+    @photo3 = create_photo(44, "photo003")
 
-    @photosets_double = double
-    Flickr::Photosets.stub(:new).and_return(@photosets_double)
+    @photosets = double
+    Flickr::Photosets.stub(:new).and_return(@photosets)
 
-    @photoset_double = double
-    Flickr::Photosets::Photoset.stub(:new).and_return(@photoset_double)
+    @photoset = double
+    Flickr::Photosets::Photoset.stub(:new).and_return(@photoset)
   end
 
   let(:folder_path) { File.join(File.dirname(__FILE__), "example_photoset") }
@@ -27,43 +28,53 @@ describe FlickrUploader::Uploader do
   context "add photo to new set" do
 
     before do
-      @photosets_double.stub(:get_list).and_return([])
+      @photosets.stub(:get_list).and_return([])
     end
 
     it "should upload a photo" do
       # stubs
-      @uploader_double.stub(:upload).and_return(@photo_double1)
-      @photosets_double.stub(:create)
+      @photosets.stub(:create)
 
       # expectation
-      @uploader_double.should_receive(:upload).with(File.join(folder_path, "photo001.jpg"))
+      @uploader.should_receive(:upload).with(File.join(folder_path, "photo001.jpg"))
+
+      subject.upload!
+    end
+
+    it "should upload all photos" do
+      # stubs
+      @photosets.stub(:create)
+
+      # expectation
+      @uploader.should_receive(:upload).with(File.join(folder_path, "photo001.jpg"))
+      @uploader.should_receive(:upload).with(File.join(folder_path, "photo002.jpg"))
+      @uploader.should_receive(:upload).with(File.join(folder_path, "photo003.jpg"))
 
       subject.upload!
     end
 
     it "should create a new set if the set doesn't exist yet" do
       # stubs
-      @uploader_double.stub(:upload).and_return(@photo_double1)
-      @photosets_double.stub(:create)
+      @photosets.stub(:create)
 
       # expectation
-      @photosets_double.should_receive(:create).with("example_photoset", "42")
+      @photosets.should_receive(:create).with("example_photoset", "42")
 
       subject.upload!
     end
 
     it "should add the photo to a newly created set" do
       # stubs
-      @uploader_double.stub(:upload).and_return(@photo_double1)
+      @photosets.stub(:create).and_return(@photoset)
+      @photosets.stub(:get_list).and_return([@photoset])
 
-      @photosets_double.stub(:create).and_return(@photoset_double)
-      @photosets_double.stub(:get_list).and_return([@photoset_double])
+      @photoset.stub(:title).and_return("example_photoset")
+      @photoset.stub(:get_photos).and_return([])
 
-      @photoset_double.stub(:title).and_return("example_photoset")
-      @photoset_double.stub(:get_photos).and_return([])
+      @photoset.stub(:add_photo)
 
       # expectation
-      @photoset_double.should_receive(:add_photo).at_least(:once).with("42")
+      @photoset.should_receive(:add_photo).at_least(:once).with("42")
 
       subject.upload!
     end
@@ -72,18 +83,63 @@ describe FlickrUploader::Uploader do
 
   context "add photo to existing set" do
 
-    it "should upload a photo" do
+    before do
+      @photoset.stub(:title).and_return("example_photoset")
+      @photoset.stub(:get_photos).and_return([@photo1])
+      @photosets.stub(:get_list).and_return([@photoset])
+    end
 
+    it "should upload a photo not in the set yet" do
+      # stubs
+      @uploader.stub(:upload).and_return(@photo2)
+      @photosets.stub(:create)
+      @photoset.stub(:add_photo)
+
+      # expectation
+      @uploader.should_receive(:upload).with(File.join(folder_path, "photo002.jpg"))
+
+      subject.upload!
+    end
+
+    it "should add the remaining photos to the existing set" do
+      # stubs
+      @photosets.stub(:create)
+
+      # expectation
+      @photoset.should_receive(:add_photo).with("43")
+      @photoset.should_receive(:add_photo).with("44")
+
+      subject.upload!
     end
 
     it "should not create a new set" do
+      #stubs
+      @photoset.stub(:add_photo)
 
+      @photosets.should_not_receive(:create)
+      subject.upload!
     end
 
     it "should not upload a photo that has already been uploaded in the set" do
+      #stubs
+      @photoset.stub(:add_photo)
 
+      # expectation
+      @photoset.should_not_receive(:add_photo).with("42")
+
+      subject.upload!
     end
 
+  end
+
+  private
+
+  def create_photo(id, title)
+    photo = double
+    photo.stub(:photoid).and_return(id)
+    photo.stub(:title).and_return(title)
+    @uploader.stub(:upload).with(File.join(folder_path, "#{title}.jpg")).and_return(photo)
+    photo
   end
 
 end
