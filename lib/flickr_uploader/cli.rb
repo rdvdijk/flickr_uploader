eafc7dfd60be03067d10d78022f51a99dd77cd52
Roel van Dijk <roel@rustradio.org>
2012-01-23 20:35:52 +0100

    Add command line option for the multi-uploader.

--- lib/flickr_uploader/cli.rb
+++ lib/flickr_uploader/cli.rb
@@ -8,12 +8,23 @@ module FlickrUploader
       Authorizer.authorize
     end
 
-    desc "upload [PATH]", "Upload a folder to Flickr."
+    desc "upload [PATH]", "Upload a single folder to Flickr."
     def upload(path)
+      folder?(path)
+      Uploader.new(path).upload!
+    end
+
+    desc "multi-upload [PATH]", "Upload all subfolders of a folder to Flickr."
+    def multi_upload(path)
+      folder?(path)
+      MultiUploader.new(path).upload!
+    end
+
+    private
+
+    def folder?(path)
       raise Thor::MalformattedArgumentError.new("Folder '#{path}' does not exist") unless File.exists?(path)
       raise Thor::MalformattedArgumentError.new("'#{path}' is not a folder") unless File.directory?(path)
-
-      Uploader.new(path).upload!
     end
 
   end
