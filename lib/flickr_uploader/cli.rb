5c4f7d6aea2b17df55fcfeaed63318d43b5ea83a
Roel van Dijk <roel@rustradio.org>
2012-01-23 20:57:08 +0100

    Add verbose logging option for CLI.

--- lib/flickr_uploader/cli.rb
+++ lib/flickr_uploader/cli.rb
@@ -9,22 +9,33 @@ module FlickrUploader
     end
 
     desc "upload [PATH]", "Upload a single folder to Flickr."
+    method_option :verbose, :type => :boolean, :aliases => "-v", :desc => "Show more logging."
     def upload(path)
-      folder?(path)
+      handle_options(path, options)
       Uploader.new(path).upload!
     end
 
     desc "multi-upload [PATH]", "Upload all subfolders of a folder to Flickr."
+    method_option :verbose, :type => :boolean, :aliases => "-v", :desc => "Show more logging."
     def multi_upload(path)
-      folder?(path)
+      handle_options(path, options)
       MultiUploader.new(path).upload!
     end
 
     private
 
+    def handle_options(path, options)
+      folder?(path)
+      verbose_logger if options[:verbose]
+    end
+
     def folder?(path)
       raise Thor::MalformattedArgumentError.new("Folder '#{path}' does not exist") unless File.exists?(path)
       raise Thor::MalformattedArgumentError.new("'#{path}' is not a folder") unless File.directory?(path)
+    end
+
+    def verbose_logger
+      FlickrUploader.logger.level = Logger::DEBUG
     end
 
   end
